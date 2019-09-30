1779dfb115c139594c7a1bd654a8886dfdfd6fbe
Muhammad Abu Bakar <linktobakar@gmail.com>
2019-10-01 01:47:25 +0500

    Updates

---
+++ precache-manifest.a162a75ea4e2ae5a5f0eb7d82007a32b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ad1517897c7325c3225369ac0feb61",
+    "url": "/React_Estore_App/index.html"
+  },
+  {
+    "revision": "81fb59d9aa956b960b6d",
+    "url": "/React_Estore_App/static/css/main.37ee6d32.chunk.css"
+  },
+  {
+    "revision": "776d151f5746783bc85e",
+    "url": "/React_Estore_App/static/js/2.3751b892.chunk.js"
+  },
+  {
+    "revision": "81fb59d9aa956b960b6d",
+    "url": "/React_Estore_App/static/js/main.d80b574d.chunk.js"
+  },
+  {
+    "revision": "8e46428f70287df59091",
+    "url": "/React_Estore_App/static/js/runtime~main.fa35dffb.js"
+  }
+]);
